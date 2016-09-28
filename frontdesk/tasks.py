979962df53de87c09f192f525fd9f06b0bb9e797
Gustavo Fonseca <gustavo@gfonseca.net>
2016-09-28 18:35:09 -0300

    Valida XMLs membros do pacote contra a SciELO PS

--- frontdesk/tasks.py
+++ frontdesk/tasks.py
@@ -7,6 +7,7 @@ from . import (
         utils,
 )
 
+import packtools
 from packtools import (
         stylechecker,
         utils as packtools_utils,
@@ -18,7 +19,11 @@ LOGGER = logging.getLogger(__name__)
 
 @app.task(ignore_result=True)
 def create_package_members(package_id):
-    package = models.Package.objects.get(pk=package_id)
+    try:
+        package = models.Package.objects.get(pk=package_id)
+    except models.Package.DoesNotExist as exc:
+        raise ValueError('Cannot find a package with id '
+                         '"%s".' % package_id) from exc
 
     with packtools_utils.Xray.fromfile(package.file.path) as xpack:
         with transaction.atomic():
@@ -29,7 +34,11 @@ def create_package_members(package_id):
 
 @app.task(bind=True, default_retry_delay=300, max_retries=5, ignore_result=True)
 def scan_package_for_viruses(self, package_id):
-    package = models.Package.objects.get(pk=package_id)
+    try:
+        package = models.Package.objects.get(pk=package_id)
+    except models.Package.DoesNotExist as exc:
+        raise ValueError('Cannot find a package with id '
+                         '"%s".' % package_id) from exc
 
     try:
         is_infected, details = utils.scan_file_for_viruses(package.file)
@@ -49,4 +58,59 @@ def scan_package_for_viruses(self, package_id):
     package.virus_scan_status = scan_status
     package.virus_scan_details = scan_details
     package.save()
+
+
+@app.task(ignore_result=True)
+def validate_package_member(member_id):
+    """Valida uma instância de ``PackageMember`` contra a SciELO PS.
+
+    Um membro só pode ser validado uma única vez.
+
+    Pode levantar a exceção ``ValueError`` no caso de ``member_id`` não
+    corresponder a uma instância de ``frontdesk.models.PackageMember``.
+    """
+    try:
+        member = models.PackageMember.objects.get(pk=member_id)
+    except models.PackageMember.DoesNotExist as exc:
+        raise ValueError('Cannot find a package member with id '
+                         '"%s".' % member_id) from exc
+
+    if not member.is_xml():
+        LOGGER.warning('Cannot run SPS validations against non XML files. '
+                       'Skipping "%s".', member.name)
+        return
+
+    # Garante que ``member`` ainda não foi validado.
+    # Uma maneira simples é acessar a entidade relacionada. Se essa entidade
+    # existir é sinal que a validação já foi realizada.
+    try:
+        _ = member.xml_control_attrs
+    except models.XMLMemberControlAttrs.DoesNotExist:
+        pass
+    else:
+        LOGGER.warning('Cannot run SPS validations on XML files that are '
+                       'already validated. Skipping "%s".', member.name)
+        return
+
+    with member.open() as member_file:
+        try:
+            validator = packtools.XMLValidator.parse(member_file)
+        except packtools.exceptions.PacktoolsError as exc:
+            summary = {
+                    'is_valid': False,
+                    'exception_type': type(exc).__name__,
+                    'exception_value': str(exc),
+            }
+        else:
+            summary = stylechecker.summarize(validator)
+
+    if summary['is_valid']:
+        check_status = models.XMLMEMBER_SPS_STATUS_VALID
+    else:
+        check_status = models.XMLMEMBER_SPS_STATUS_INVALID
+
+    models.XMLMemberControlAttrs.objects.create(
+            member=member,
+            sps_check_status=check_status,
+            sps_check_details=summary)
 
---
+++ frontdesk/tests/test_tasks.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+
+from frontdesk import (
+        tasks,
+        models,
+)
+from . import modelfactories
+
+
+class ValidatePackageMemberTests(TestCase):
+
+    def test_unknown_member_id_raises_value_error(self):
+        self.assertRaises(ValueError,
+                lambda: tasks.validate_package_member(10928374871234))
+
+    def test_xml_members_are_validated(self):
+        member = modelfactories.PackageMemberFactory(
+                name='0004-2730-aem-60-4/0004-2730-aem-60-4-0407.xml')
+
+        _ = tasks.validate_package_member(member.pk)
+
+        self.assertIsInstance(member.xml_control_attrs,
+                models.XMLMemberControlAttrs)
+
+    def test_validation_results_are_saved_correctely(self):
+        member = modelfactories.PackageMemberFactory(
+                name='0004-2730-aem-60-4/0004-2730-aem-60-4-0407.xml')
+
+        _ = tasks.validate_package_member(member.pk)
+
+        self.assertTrue(member.xml_control_attrs.sps_check_status in
+                [models.XMLMEMBER_SPS_STATUS_VALID, models.XMLMEMBER_SPS_STATUS_INVALID])
+        self.assertIsInstance(member.xml_control_attrs.sps_check_details,
+                dict)
+
+    def test_non_xml_members_are_skipped(self):
+        """Testamos que a instância de ``models.XMLMemberControlAttrs`` não
+        é criada e associada ao atributo ``member.xml_control_attrs``.
+        """
+        member = modelfactories.PackageMemberFactory(
+                name='0004-2730-aem-60-4/0004-2730-aem-60-4-0407.pdf')
+
+        _ = tasks.validate_package_member(member.pk)
+
+        self.assertRaises(models.XMLMemberControlAttrs.DoesNotExist,
+                lambda: member.xml_control_attrs)
+
