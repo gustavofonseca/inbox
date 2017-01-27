e75763f2afca01ea523a8348a4c0ebd24b9fa67b
Gustavo Fonseca <gustavo@gfonseca.net>
2017-01-27 15:16:45 -0200

    Define *gevent* como o tipo de worker do Gunicorn.
    O número de workers será definido dinamicamente segundo a fórmula
    ``2 * total de cpus``.

--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 python /app/manage.py migrate #todo: reavaliar a execução automática
 python /app/manage.py collectstatic --noinput
-/usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app --log-level=$DJANGO_LOG_LEVEL --timeout=0
+/usr/local/bin/gunicorn config.wsgi \
+  -k gevent \
+  -w $(python -c "import multiprocessing; print(2 * multiprocessing.cpu_count())") \
+  -b 0.0.0.0:5000 \
+  --chdir=/app \
+  --log-level=$DJANGO_LOG_LEVEL \
+  --timeout=0
+
